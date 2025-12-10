04c7534da9ffeb47efa87c8b894b47cf6bdd41d3
Ahmed Rehman <ahmedrehman0010@gmail.com>
2025-12-11 04:14:01 +0500

    updated client view

--- MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044e9c4303161fe672b19d1598d3ba3657601781")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83855f8eb722843a901a18f3cee515d64f6af225")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
